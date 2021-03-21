a3c227dfc6d27f3c7e606ea8ba49bc761d27c142
PDA2304 <isip_d.a.pahomov@mpt.ru>
2021-03-21 11:13:06 +0300

    Добавьте файлы проекта.

---
+++ PDAManga/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PDAManga.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
